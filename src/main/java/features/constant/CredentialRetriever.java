35399ca9a503890c03eec7f2498a58ddebfffd37
LonelyProgrammer21 <jeric_tayson@protonmail.com>
2022-04-01 08:01:14 +0800

    Added XML files and retriever class.

---
+++ src/main/java/features/constant/CredentialRetriever.java
@@ -0,0 +1,31 @@
+package features.constant;
+
+public class CredentialRetriever {
+
+    private String TOKEN;
+    public static String V01 = "";
+    public static String OURGUILDID = "";
+
+    private static void getEnv() throws Exception{
+
+            V01 = System.getenv("V01");
+            OURGUILDID = System.getenv("OURGUILDID");
+    }
+
+    public CredentialRetriever(){
+
+        try{
+
+            TOKEN = System.getenv("TOKEN");
+            getEnv();
+        }catch (Exception e){
+
+            System.out.printf("Error on getting ENV's StackTrace: %s%n", e.getMessage());
+        }
+    }
+
+    public String retrieveFile(){
+
+        return TOKEN;
+    }
+}
