6b1e47ac29a50fd7b75ffdc0384071db2380a344
LonelyProgrammer21 <jeric_tayson@protonmail.com>
2022-04-20 08:16:37 +0800

    Removed Unnecessary values.

--- src/main/java/features/constant/ConstantValues.java
+++ src/main/java/features/constant/ConstantValues.java
@@ -21,26 +21,7 @@ public final class ConstantValues {
     public static final String[] CHEERINGEMOTE = {"✽-(ˆ▽ˆ)/✽ ✽\\(ˆ▽ˆ)-✽", "q(❂‿❂)p", "o((*^▽^*))o","⌒°(❛ᴗ❛)°⌒",
     "o(*^▽^*)o", "o(*>ω<*)o", "o(〃＾▽＾〃)o", "(/^▽^)/", "ヾ（〃＾∇＾）ﾉ♪"};
 
-    public static final String[] MONTHS = {"January", "Febuary", "March", "April", "May", "June", "July", "August", "September",
-    "October", "November", "December"};
 
-    public static final String[] BLOODGIFS = {"https://c.tenor.com/NIgywfrc6AoAAAAC/blood-anime.gif",
-            "https://i.pinimg.com/originals/d6/82/db/d682db7fd04d4de2c9ecbacfc073fcac.gif",
-            "https://giffiles.alphacoders.com/170/170905.gif","https://c.tenor.com/_9xv0QLnvn4AAAAd/diabolik-blood.gif",
-            "https://qph.fs.quoracdn.net/main-qimg-56e5cd36ea29abdd5b981a9777c0669b"};
-
-    public static final String[] HUGGIFS = {"https://25.media.tumblr.com/tumblr_ma7l17EWnk1rq65rlo1_500.gif",
-            "https://c.tenor.com/Gpj9hIIb6vIAAAAM/full-charged-yagakimi.gif","https://c.tenor.com/ItpTQW2UKPYAAAAM/cuddle-hug.gif"
-            ,"https://c.tenor.com/aC5zO5ZTruEAAAAC/cuddle-snuggle.gif", "https://c.tenor.com/4n3T2I239q8AAAAC/anime-cute.gif"
-            ,"https://c.tenor.com/BmbTYhCZ5UsAAAAM/yuri-sleeping-yuri-sleep.gif","https://c.tenor.com/hGUWkkHB_DQAAAAM/cuddle-anime.gif"};
-
-    public static final String[] KISSGIFS = {"https://c.tenor.com/HgV0doOr_YoAAAAC/golden-time-anime.gif", "https://i.gifer.com/8Sbz.gif",
-    "https://i.gifer.com/FChS.gif", "https://acegif.com/wp-content/uploads/anime-kissin-15.gif", ""};
-
-    public static final String[] CHEERINGGIF = {"https://thumbs.gfycat.com/FlakyBlackandwhiteDassierat-size_restricted.gif",
-    "https://c.tenor.com/SpT5l-6LQtAAAAAC/anime-happy.gif", "https://i.gifer.com/78I.gif", "https://i.pinimg.com/originals/46/23/7d/46237d02bdb88efe9fcb0845bba0b039.gif",
-    "https://c.tenor.com/fKjC8KynhWYAAAAC/anime-anime-cheerleader.gif", "https://c.tenor.com/55nAR-tZfYcAAAAC/ochako-anime.gif"
-            , "https://c.tenor.com/e5k8N62tb58AAAAd/touhou-aya-shameimaru.gif"};
 
     public static final Color[] COLORS = {Color.CYAN, Color.RED, Color.BLUE, Color.WHITE, Color.DARK_GRAY,
             Color.MAGENTA, Color.YELLOW, Color.PINK, Color.GREEN, Color.ORANGE, Color.blue};
