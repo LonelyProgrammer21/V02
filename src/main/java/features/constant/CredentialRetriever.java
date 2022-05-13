e5546db14b01479bf2b47811d1ed4ef7233fe735
LonelyProgrammer21 <jeric_tayson@protonmail.com>
2022-05-13 09:11:02 +0800

    Added tokens for my default configuration on my guild.

--- src/main/java/features/constant/CredentialRetriever.java
+++ src/main/java/features/constant/CredentialRetriever.java
@@ -4,14 +4,18 @@ public class CredentialRetriever {
 
     private String TOKEN;
     public static String V01 = "";
-    public static String OURGUILDID = "";
-    public static String TENOR_TOKEN = "";
+    public static String OURGUILDID;
+    public static String TENOR_TOKEN;
+    public static String HACKERRIST;
+    public static String DEFAULT_TEXT_CHANNEL;
 
     private static void getEnv() {
 
             V01 = System.getenv("V01");
             OURGUILDID = System.getenv("OURGUILDID");
             TENOR_TOKEN = System.getenv("TENOR_TOKEN");
+            DEFAULT_TEXT_CHANNEL = System.getenv("DEFAULT_TEXT_CHANNEL");
+            HACKERRIST = System.getenv("HACKERRIST");
     }
 
     public CredentialRetriever(){
